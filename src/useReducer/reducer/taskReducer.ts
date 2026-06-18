abed57b7019cd1d7d7aa681d7670d489744c7295
Cristian Lopez <crca9623@gmail.com>
2026-06-18 13:45:21 -0500

    create component TodoApp and useReducer

---
+++ src/useReducer/reducer/taskReducer.ts
@@ -0,0 +1,210 @@
+/**
+ * Representa una tarea dentro de la aplicación
+ */
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+/**
+ * Estado gestionado por el reducer de tareas.
+ *
+ * Este objeto representa toda la información que el reducer
+ * necesita para calcular el siguiente estado de la aplicación.
+ *
+ * Propiedades:
+ * - todos: lista de tareas registradas.
+ * - total: número total de tareas.
+ * - completed: número de tareas completadas.
+ * - pending: número de tareas pendientes.
+ */
+interface TaskState {
+  todos: Todo[]
+  total: number
+  completed: number
+  pending: number
+}
+
+/**
+ * Acciones que pueden modificar el estado.
+ *
+ * ADD_TODO:
+ * - Agrega una nueva tarea.
+ * - payload: texto de la tarea.
+ *
+ * TOGGLE_TODO:
+ * - Cambia el estado completed de una tarea.
+ * - payload: id de la tarea.
+ *
+ * DELETE_TODO:
+ * - Elimina una tarea.
+ * - payload: id de la tarea.
+ */
+export type TaskAction = 
+| {type: 'ADD_TODO', payload: string}
+| {type: 'TOGGLE_TODO', payload: number}
+| {type: 'DELETE_TODO', payload: number}
+
+export const getTaskInitialState = () => {
+  return {
+    todos: [],
+    total: 0,
+    completed: 0,
+    pending: 0
+  }
+}
+/**
+ * Reducer encargado de gestionar el estado de las tareas.
+ *
+ * Flujo:
+ * 1. Recibe el estado actual.
+ * 2. Recibe una acción.
+ * 3. Evalúa el tipo de acción.
+ * 4. Calcula un nuevo estado.
+ * 5. Retorna el nuevo estado.
+ *
+ * Importante:
+ * - Nunca modifica el estado original.
+ * - Siempre retorna un nuevo objeto.
+ */
+export const taskReducer = (
+  state: TaskState, 
+  action: TaskAction
+):TaskState => {
+
+  switch (action.type) {
+
+    /**
+     * ADD_TODO
+     *
+     * Responsabilidad:
+     * - Crear una nueva tarea.
+     * - Agregarla al arreglo de tareas.
+     * - Actualizar las métricas del estado.
+     * - Retornar el nuevo estado.
+     *
+     * payload:
+     * - Texto de la nueva tarea.
+     */
+    case 'ADD_TODO':{
+
+       // Construye el nuevo objeto Todo.
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload.trim(),
+        completed: false
+      }
+
+      /**
+       * Importante:
+       * Nunca modificamos state.todos directamente.
+       *
+       * Creamos un nuevo arreglo utilizando
+       * el operador spread (...).
+       */
+      return {
+        ...state,
+
+        // Agrega la nueva tarea al final del arreglo.
+        todos: [...state.todos, newTodo],
+
+        // Actualiza las estadísticas.
+        total: state.todos.length + 1,
+        pending: state.pending + 1
+      }
+    }
+
+    /**
+     * DELETE_TODO
+     *
+     * Responsabilidad:
+     * - Eliminar una tarea por id.
+     * - Recalcular las métricas del estado.
+     * - Retornal el nuevo estado.
+     * 
+     * payload:
+     * - Id de la tarea a eliminar.
+     */
+    case 'DELETE_TODO': {
+
+      /**
+       * filter() crea un nuevo arreglo
+       * excluyendo la tarea seleccionada.
+       */
+      const currentTodos = state.todos.filter(
+        todo => todo.id !== action.payload
+      )
+
+      /**
+     * Recalcular estadísticas.
+     *
+     * completedTodos:
+     * - Cantidad de tareas completadas.
+     *
+     * pendingTodos:
+     * - Cantidad de tareas pendientes.
+     */
+      const completedTodos = currentTodos.filter(todo => todo.completed).length
+      const pendingTodos = currentTodos.length - completedTodos
+
+      return {
+        ...state,
+        todos: currentTodos,
+        total: currentTodos.length,
+        completed: completedTodos,
+        pending: pendingTodos
+      }
+    }
+
+    /**
+     * TOGGLE_TODO
+     * 
+     * Responsabilidad:
+     * - Cambiar el estado completed de una tarea.
+     * 
+     * Payload: 
+     * - Id de la tarea a actualizar.
+     */
+    case 'TOGGLE_TODO': {
+
+      /**
+       * map() crea un nuevo arreglo.
+       * 
+       * Si encuentra la tarea indicada,
+       * invierte el valor del completed.
+       */
+      const updatedTodos = state.todos.map(todo => {
+
+        if(todo.id === action.payload) {
+          return {
+            ...todo, 
+
+            // true -> false
+            // false -> true
+            completed: !todo.completed}
+        }
+
+        return todo
+      })
+
+      /**
+       * Recalcular estadísticas.
+       */
+      const completedTodos = updatedTodos.filter(todo => todo.completed).length
+      const pendingTodos = updatedTodos.length - completedTodos
+
+      return {
+        ...state,
+        todos: updatedTodos,
+        completed: completedTodos,
+        pending: pendingTodos
+      }
+    }
+
+    default:
+      break;
+  }
+
+  return state
+}
