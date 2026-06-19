081a2f67fe9c42b0b60e8b88d77bcf8f332544a7
Cristian Lopez <crca9623@gmail.com>
2026-06-19 07:10:07 -0500

    New componente ScrambleWords only use useState

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,13 @@ import { createRoot } from 'react-dom/client'
 // import { TrafficLightWithEffectHook } from './useEffect/TrafficLightWithEffectHook'
 // import { PokemonPage } from './Pokemon/PokemonPage'
 // import { FocusScreen } from './useRef/FocusScreen'
-import { TasksApp } from './useReducer/TasksApp'
+// import { TasksApp } from './useReducer/TasksApp'
+import { ScrambleWords } from './useReducer/ScrambleWords'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TasksApp />
+    <ScrambleWords />
   </StrictMode>,
 )
