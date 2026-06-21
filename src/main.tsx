e4d3d92d7eeac55f9a000506a93e87b0dabf8294
Cristian Lopez <crca9623@gmail.com>
2026-06-21 09:27:01 -0500

    useOptimistic and useTransition

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { Toaster } from "sonner";
+
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './useState/TrafficLight'
 // import { TrafficLightWithEffect } from './useEffect/TrafficLightWithEffect'
@@ -10,12 +12,14 @@ import { createRoot } from "react-dom/client";
 // import { TasksApp } from './useReducer/TasksApp'
 // import { ScrambleWords } from './useReducer/ScrambleWords'
 // import { MemoHook } from './memos/MemoHook'
-import { MemoCounter } from "./memos/MemoCounter";
+// import { MemoCounter } from "./memos/MemoCounter";
+import { InstagromApp } from "./useOptimistic/IntagromApp";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MemoCounter />
+    <Toaster />
+    <InstagromApp />
   </StrictMode>,
 );
