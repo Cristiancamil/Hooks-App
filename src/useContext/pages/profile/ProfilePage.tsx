cb592d63c955b9e7245671751967cc5fd6f7f154
Cristian Lopez <crca9623@gmail.com>
2026-06-22 16:32:27 -0500

    Create project useContext

--- src/useContext/pages/profile/ProfilePage.tsx
+++ src/useContext/pages/profile/ProfilePage.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { UserContext } from "@/useContext/context/UserContext";
+import { use } from "react";
+
 export const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { user, logout } = use(UserContext);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl">Perfil del usuario</h1>
+
+      <pre className="my-4 w-[70%] overflow-x-auto">
+        {JSON.stringify(user, null, 2)}
+      </pre>
+
+      <Button variant="destructive" onClick={logout}>
+        Salir
+      </Button>
+    </div>
+  );
 };
